c4f8e09c26125e1ae66708feb2884302c7a729c0
luispfatdo <luis.paulo.tdo@gmail.com>
2026-06-22 18:51:32 -0300

    Criando o DbContext.

---
+++ src/PagBoleto.Infrastructure/Persistence/PagBoletoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PagBoleto.Domain.Entities;
+
+namespace PagBoleto.Infrastructure.Persistence;
+
+public class PagBoletoDbContext : DbContext
+{
+    public PagBoletoDbContext(DbContextOptions<PagBoletoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Boleto> Boletos => Set<Boleto>();
+    public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PagBoletoDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
