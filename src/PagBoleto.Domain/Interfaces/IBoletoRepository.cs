dddae651512bb29954900c438206b80148e0dc41
luispfatdo <luis.paulo.tdo@gmail.com>
2026-06-24 10:28:30 -0300

    Definindo a interface do BoletoRepository.

---
+++ src/PagBoleto.Domain/Interfaces/IBoletoRepository.cs
@@ -0,0 +1,10 @@
+﻿using PagBoleto.Domain.Entities;
+
+namespace PagBoleto.Domain.Interfaces;
+
+public interface IBoletoRepository
+{
+    Task AdicionarAsync(Boleto boleto, CancellationToken cancellationToken);
+    Task<Boleto?> ObterPorIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<IReadOnlyList<Boleto>> ListarAsync(CancellationToken cancellationToken);
+}
