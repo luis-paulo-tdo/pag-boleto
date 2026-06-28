76321ef6e9fb46080b3612a52ffb69824c0143e2
luispfatdo <luis.paulo.tdo@gmail.com>
2026-06-28 04:09:52 -0300

    Criação do Unit Of Work apenas como forma de abstrair a dependência com o Contexto na Infraestrutura.

---
+++ src/PagBoleto.Application/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace PagBoleto.Application.Abstractions;
+
+public interface IUnitOfWork
+{
+    Task<int> CommitAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/PagBoleto.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using PagBoleto.Application.Abstractions;
+
+namespace PagBoleto.Infrastructure.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly PagBoletoDbContext _context;
+
+    public UnitOfWork(PagBoletoDbContext context) => _context = context;
+
+    public Task<int> CommitAsync(CancellationToken cancellationToken = default) => _context.SaveChangesAsync(cancellationToken);
+}
