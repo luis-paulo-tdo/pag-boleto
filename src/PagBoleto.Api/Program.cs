017103bcff8caad5f3ef44ee2a63e1bd3ffaf51a
luispfatdo <luis.paulo.tdo@gmail.com>
2026-06-29 07:36:46 -0300

    Chamando a extensão na Program e removendo o weatherforecast.

--- src/PagBoleto.Api/Program.cs
+++ src/PagBoleto.Api/Program.cs
@@ -1,9 +1,7 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
-using PagBoleto.Application.Abstractions;
-using PagBoleto.Domain.Interfaces;
+using PagBoleto.Infrastructure;
 using PagBoleto.Infrastructure.Persistence;
-using PagBoleto.Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,8 +18,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IBoletoRepository, BoletoRepository>();
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
@@ -33,28 +30,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
