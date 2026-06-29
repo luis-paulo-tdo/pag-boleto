a44f32ccf84daafa9b506e16d81f46c5bf2afd20
luispfatdo <luis.paulo.tdo@gmail.com>
2026-06-29 07:37:04 -0300

    Criando a casca do handler para criar boleto.

---
+++ src/PagBoleto.Application/Boletos/Commands/CriarBoletoCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using PagBoleto.Domain.Interfaces;
+
+namespace PagBoleto.Application.Boletos.Commands;
+
+public record CriarBoletoCommand(string LinhaDigitavel, decimal Valor, DateOnly Vencimento);
+
+public class CriarBoletoCommandHandler
+{
+    private readonly IBoletoRepository _boletoRepository;
+}
