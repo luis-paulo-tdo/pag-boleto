632f2332892a0e34bf935abcc2f84d4c9d706bd5
luispfatdo <luis.paulo.tdo@gmail.com>
2026-06-29 07:35:16 -0300

    Criando OutboxMessageRepository.

---
+++ src/PagBoleto.Domain/Interfaces/IOutboxMessageRepository.cs
@@ -0,0 +1,8 @@
+﻿using PagBoleto.Domain.Entities;
+
+namespace PagBoleto.Domain.Interfaces;
+
+public interface IOutboxMessageRepository
+{
+    Task AdicionarAsync(OutboxMessage outboxMessage, CancellationToken cancellationToken = default);
+}
---
+++ src/PagBoleto.Infrastructure/Persistence/Repositories/OutboxMessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using PagBoleto.Domain.Entities;
+using PagBoleto.Domain.Interfaces;
+
+namespace PagBoleto.Infrastructure.Persistence.Repositories;
+
+public class OutboxMessageRepository : IOutboxMessageRepository
+{
+    private readonly PagBoletoDbContext _context;
+
+    public OutboxMessageRepository(PagBoletoDbContext context) => _context = context;
+
+    public Task AdicionarAsync(OutboxMessage outboxMessage, CancellationToken cancellationToken = default)
+    {
+        _context.OutboxMessages.Add(outboxMessage);
+        return Task.CompletedTask;
+    }
+}
