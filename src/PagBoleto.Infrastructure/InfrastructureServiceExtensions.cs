dfa07eddb329dd4106620940eda3244b1725f1c5
luispfatdo <luis.paulo.tdo@gmail.com>
2026-06-29 07:36:15 -0300

    Criando extension para registrar as Repositories e o UoW no contêiner de DI.

---
+++ src/PagBoleto.Infrastructure/InfrastructureServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PagBoleto.Application.Abstractions;
+using PagBoleto.Domain.Interfaces;
+using PagBoleto.Infrastructure.Persistence;
+using PagBoleto.Infrastructure.Persistence.Repositories;
+
+namespace PagBoleto.Infrastructure;
+
+public static class InfrastructureServiceExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IBoletoRepository, BoletoRepository>();
+        services.AddScoped<IOutboxMessageRepository, OutboxMessageRepository>();
+
+        return services;
+    }
+}
